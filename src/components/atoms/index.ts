fd38e87da23bf88def54f2ad7ccb20366db0ba22
cyeju <yeju1019@gmail.com>
2024-01-07 19:46:00 +0900

    feat(fourth): fourth 컴포넌트 개발

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,5 +2,6 @@ export * from './BoxBorder';
 export * from './Card';
 export * from './ColorSection';
 export * from './DetailText';
+export * from './GlassCard';
 export * from './SubText';
 export * from './Title';
--- src/components/organisms/Fourth.tsx
+++ src/components/organisms/Fourth.tsx
@@ -1,6 +1,58 @@
-import type { FC } from 'react';
-import { ColorSection } from '@/components/atoms/ColorSection';
+'use client';
+
+import { useState, type FC, type SyntheticEvent } from 'react';
+import { Stack, Box, Tabs, Tab } from '@mui/material';
+import { CustomTabPanel } from '@/components/molecules';
+import { ColorSection } from '@/components/atoms';
+
+const a11yProps = (index: number) => {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+};
 
 export const Fourth: FC = () => {
-    return <ColorSection color="#0f4c82">fourth</ColorSection>;
+    const [value, setValue] = useState(0);
+
+    const handleChange = (_event: SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <ColorSection color="#0f4c82">
+            <Stack sx={{ alignItems: 'center' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="basic tabs example"
+                        TabIndicatorProps={{
+                            style: {
+                                backgroundColor: '#f7e057',
+                            },
+                        }}
+                        sx={{
+                            '.Mui-selected': {
+                                color: `#f7e057 !important`,
+                            },
+                        }}
+                    >
+                        <Tab label="개발철학" {...a11yProps(0)} sx={{ color: 'white' }} />
+                        <Tab label="Culture Fit" {...a11yProps(1)} sx={{ color: 'white' }} />
+                        <Tab label="관심사/기타" {...a11yProps(2)} sx={{ color: 'white' }} />
+                    </Tabs>
+                </Box>
+                <CustomTabPanel value={value} index={0}>
+                    개발 철학
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={1}>
+                    중요하게 생각하는 점,
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={2}>
+                    관심사 기타
+                </CustomTabPanel>
+            </Stack>
+        </ColorSection>
+    );
 };
