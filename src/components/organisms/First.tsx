810d6b245ea8e567d5d6ca0eda10080f74fd792d
cyeju <yeju1019@gmail.com>
2024-01-07 21:14:28 +0900

    style(first): style 재사용성을 위해서 분리

--- src/components/organisms/First.tsx
+++ src/components/organisms/First.tsx
@@ -4,25 +4,54 @@ import type { FC } from 'react';
 import { Typography } from '@mui/material';
 import { ColorSection } from '@/components/atoms';
 
+const style = {
+    image: {
+        position: 'absolute',
+        zIndex: '100',
+        left: '50%',
+        top: '29%',
+        height: '300px',
+    },
+    h2: {
+        position: 'absolute',
+        zIndex: '101',
+        left: '50%',
+        top: '55%',
+        transform: 'translate(-50%,-10%)',
+        width: '100%',
+        fontSize: {
+            xs: '1.9rem',
+            sm: '3.75rem',
+        },
+    },
+} as const;
+
 export const First: FC = () => {
     return (
         <ColorSection color="#BD3455">
-            <Typography component="h1" variant="h1">
-                FRONTEND
-            </Typography>
             <Typography
-                component="h2"
-                variant="h2"
+                component="h1"
+                variant="h1"
                 sx={{
-                    position: 'absolute',
-                    zIndex: '101',
-                    left: '50%',
-                    top: '55%',
-                    transform: 'translate(-50%,-10%)',
-                    width: '100%',
+                    fontSize: {
+                        xs: '3rem',
+                        sm: '6rem',
+                    },
                 }}
             >
-                <span style={{ color: '#f7e057' }}>개발자 최예주</span>에요
+                FRONTEND
+            </Typography>
+
+            <Typography component="h2" variant="h2" sx={style.h2}>
+                <span
+                    style={{
+                        textShadow:
+                            ' 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #f7e057,0 0 92px #f7e057, 0 0 102px #f7e057, 0 0 112px #f7e057, 0 0 161px #f7e057',
+                    }}
+                >
+                    개발자 최예주
+                </span>
+                에요
             </Typography>
         </ColorSection>
     );
