fa5993b64480b806707dc5411eb75e4ea3c97c72
cyeju <yeju1019@gmail.com>
2024-01-07 18:20:59 +0900

    feat(first): ui 컴포넌트 개발

--- src/components/organisms/First.tsx
+++ src/components/organisms/First.tsx
@@ -1,6 +1,29 @@
+'use client';
+
 import type { FC } from 'react';
-import { ColorSection } from '@/components/atoms/ColorSection';
+import { Typography } from '@mui/material';
+import { ColorSection } from '@/components/atoms';
 
 export const First: FC = () => {
-    return <ColorSection color="#BD3455">first</ColorSection>;
+    return (
+        <ColorSection color="#BD3455">
+            <Typography component="h1" variant="h1">
+                FRONTEND
+            </Typography>
+            <Typography
+                component="h2"
+                variant="h2"
+                sx={{
+                    position: 'absolute',
+                    zIndex: '101',
+                    left: '50%',
+                    top: '55%',
+                    transform: 'translate(-50%,-10%)',
+                    width: '100%',
+                }}
+            >
+                <span style={{ color: '#f7e057' }}>개발자 최예주</span>에요
+            </Typography>
+        </ColorSection>
+    );
 };
