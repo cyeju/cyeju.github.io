4b75f4cde41f8883b37902401aea036e8ac6ae98
cyeju <yeju1019@gmail.com>
2024-01-07 17:16:01 +0900

    feat(colorsection): colorsection 컴포넌트 개발

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
+import { ColorSection } from '@/components/atoms/ColorSection';
 
 const Home: NextPage = () => {
-    return <>Hi</>;
+    return <ColorSection color="#BD3455">Hi</ColorSection>;
 };
 
 export default Home;
