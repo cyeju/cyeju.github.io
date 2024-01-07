3040bf69344d169e5e2674bfe0df900a3f092e54
cyeju <yeju1019@gmail.com>
2024-01-07 21:57:05 +0900

    feat(footer): footer ui 컴포넌트  개발

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next';
 import { HomeTemplate } from '@/components/templates';
+import { Footer } from '@/components/organisms/Footer';
 
 const Home: NextPage = () => {
-    return <HomeTemplate />;
+    return (
+        <>
+            <HomeTemplate />
+            <Footer />
+        </>
+    );
 };
 
 export default Home;
---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,33 @@
+import type { FC } from 'react';
+import { Container } from '@mui/material';
+
+const style = {
+    container: {
+        py: '30px',
+        textAlign: 'center',
+        color: 'white',
+        backgroundColor: '#333',
+        minWidth: '100%',
+    },
+    link: {
+        textDecoration: 'none',
+        color: '#f7e057',
+        marginRight: '3px',
+    },
+} as const;
+
+export const Footer: FC = () => {
+    return (
+        <Container component="footer" sx={style.container}>
+            <a
+                href="https://github.com/brom5033/brom5033.github.io/blob/main/LICENSE"
+                target="_blank"
+                rel="noreferrer"
+                style={style.link}
+            >
+                MIT LICENSE
+            </a>
+            2024 by cyeju Yeju Choi
+        </Container>
+    );
+};
