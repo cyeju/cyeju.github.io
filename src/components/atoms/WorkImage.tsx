efada2775d6519e0b4c7eddb2841c12389f59454
cyeju <yeju1019@gmail.com>
2024-01-29 02:55:39 +0900

    build(next.config): image 값 수정

--- src/components/atoms/WorkImage.tsx
+++ src/components/atoms/WorkImage.tsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react';
 import { type FC } from 'react';
-// import Image from 'next/image';
+import Image from 'next/image';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 // import { motion } from 'framer-motion';
 // import { Typography } from '@mui/material';
@@ -10,30 +10,30 @@ interface Props {
     title: string;
 }
 
-// const style = {
-//     title: {
-//         position: 'absolute',
-//         zIndex: '102',
-//         color: 'white',
-//         textShadow: '0px 0px 32px black',
-//     },
-//     imgWrap: {
-//         marginBottom: 10,
-//         overflow: 'hidden',
-//         width: 500,
-//         height: 300,
-//     },
-//     img: {
-//         objectFit: 'cover',
-//         filter: 'grayscale(0.7)',
-//         transition: '0.3s',
-//     },
-//     img_hover: {
-//         filter: 'grayscale(0) brightness(1)',
-//         transform: 'scale(1.1)',
-//         transition: '0.2s',
-//     },
-// } as const;
+const style = {
+    title: {
+        position: 'absolute',
+        zIndex: '102',
+        color: 'white',
+        textShadow: '0px 0px 32px black',
+    },
+    imgWrap: {
+        marginBottom: 10,
+        overflow: 'hidden',
+        width: 500,
+        height: 300,
+    },
+    img: {
+        objectFit: 'cover',
+        filter: 'grayscale(0.7)',
+        transition: '0.3s',
+    },
+    img_hover: {
+        filter: 'grayscale(0) brightness(1)',
+        transform: 'scale(1.1)',
+        transition: '0.2s',
+    },
+} as const;
 
 export const WorkImage: FC<Props> = ({ src, title }) => {
     // const [isHovering, setIsHovered] = useState(false);
@@ -55,18 +55,18 @@ export const WorkImage: FC<Props> = ({ src, title }) => {
                 >
                     {title}
                 </Typography>
-            )}
+            )} */}
             <div style={style.imgWrap}>
                 <Image
                     alt="이미지"
                     src={src}
-                    style={isHovering ? { ...style.img, ...style.img_hover } : style.img}
+                    // style={isHovering ? { ...style.img, ...style.img_hover } : style.img}
                     width={500}
                     height={300}
-                    onMouseEnter={onMouseEnter}
-                    onMouseLeave={onMouseLeave}
+                    // onMouseEnter={onMouseEnter}
+                    // onMouseLeave={onMouseLeave}
                 />
-            </div> */}
+            </div>
         </>
     );
 };
