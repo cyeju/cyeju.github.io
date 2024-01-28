c1a97e2415e0927f8f01bc2aa0a3f7c27fa4baec
cyeju <yeju1019@gmail.com>
2024-01-29 02:32:18 +0900

    build(next.config): output 수정

--- src/components/atoms/WorkImage.tsx
+++ src/components/atoms/WorkImage.tsx
@@ -1,47 +1,50 @@
-import { useState, type FC } from 'react';
-import Image from 'next/image';
+// import { useState } from 'react';
+import { type FC } from 'react';
+// import Image from 'next/image';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
-import { motion } from 'framer-motion';
-import { Typography } from '@mui/material';
+// import { motion } from 'framer-motion';
+// import { Typography } from '@mui/material';
 
 interface Props {
     src: string | StaticImport;
     title: string;
 }
 
-const style = {
-    title: {
-        position: 'absolute',
-        zIndex: '102',
-        color: 'white',
-        textShadow: '0px 0px 32px black',
-    },
-    imgWrap: {
-        marginBottom: 10,
-        overflow: 'hidden',
-        width: 500,
-        height: 300,
-    },
-    img: {
-        objectFit: 'cover',
-        filter: 'grayscale(0.7)',
-        transition: '0.3s',
-    },
-    img_hover: {
-        filter: 'grayscale(0) brightness(1)',
-        transform: 'scale(1.1)',
-        transition: '0.2s',
-    },
-} as const;
+// const style = {
+//     title: {
+//         position: 'absolute',
+//         zIndex: '102',
+//         color: 'white',
+//         textShadow: '0px 0px 32px black',
+//     },
+//     imgWrap: {
+//         marginBottom: 10,
+//         overflow: 'hidden',
+//         width: 500,
+//         height: 300,
+//     },
+//     img: {
+//         objectFit: 'cover',
+//         filter: 'grayscale(0.7)',
+//         transition: '0.3s',
+//     },
+//     img_hover: {
+//         filter: 'grayscale(0) brightness(1)',
+//         transform: 'scale(1.1)',
+//         transition: '0.2s',
+//     },
+// } as const;
 
 export const WorkImage: FC<Props> = ({ src, title }) => {
-    const [isHovering, setIsHovered] = useState(false);
-    const onMouseEnter = () => setIsHovered(true);
-    const onMouseLeave = () => setIsHovered(false);
+    // const [isHovering, setIsHovered] = useState(false);
+    // const onMouseEnter = () => setIsHovered(true);
+    // const onMouseLeave = () => setIsHovered(false);
 
     return (
         <>
-            {isHovering && (
+            {src}
+            {title}
+            {/* {isHovering && (
                 <Typography
                     component={motion.h4}
                     variant="h4"
@@ -63,7 +66,7 @@ export const WorkImage: FC<Props> = ({ src, title }) => {
                     onMouseEnter={onMouseEnter}
                     onMouseLeave={onMouseLeave}
                 />
-            </div>
+            </div> */}
         </>
     );
 };
